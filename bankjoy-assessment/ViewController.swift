e858627f01f2f0794124b0bd25c0fd7f9f645bfe
Nii <jbnii@outlook.com>
2025-01-04 22:53:38 -0330

    Initial Commit

---
+++ bankjoy-assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bankjoy-assessment
+//
+//  Created by Jude Botchwey on 2025-01-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
